2fc11279a5d2690af57b7726ccb8b7a7d128e8a2
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-07-10 14:44:45 +0200

    Ajout EX1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,25 @@
+package ex1;
+
+/**
+ * 
+ * @author Damien Mouchagues
+ * 
+ * Cette classe permet d'effectuer différents calculs
+ */
+public class Calcul {
+	
+	/*
+	 * Retourne l'addition de deux nombres
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/*
+	 * Retourne la soustraction de deux nombres
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,26 @@
+package ex1;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author Damien Mouchagues
+ * Classe gérant des entreprises
+ */
+public class Entreprise {
+	
+	public int siret;
+	public String nom;
+	public String adresse;
+	public Date dateCreation;
+	
+	public static final int CAPITAL_MAX = 3_000_000;
+	
+	/**
+	 * Affiche le statut de l'entreprise
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
